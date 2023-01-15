e73da1ae3394b029207c323106de4ace654a046c
MihMihai13 <110050243+MihMihai13@users.noreply.github.com>
2023-01-15 14:25:07 +0200

    first commit

---
+++ overviewerConfig.js
@@ -0,0 +1,100 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "My world"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1672782826",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "day",
+            "zoomLevels": 8,
+            "defaultZoom": 1,
+            "maxZoom": 8,
+            "path": "day",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "My world",
+            "last_rendertime": 1659557767,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                239,
+                144,
+                189
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                239,
+                144,
+                189
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "night",
+            "zoomLevels": 8,
+            "defaultZoom": 1,
+            "maxZoom": 8,
+            "path": "night",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "My world",
+            "last_rendertime": 1659557767,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                239,
+                144,
+                189
+            ],
+            "lastrenderversion": 2,
+            "minZoom": 0,
+            "spawn": [
+                239,
+                144,
+                189
+            ],
+            "north_direction": 0
+        }
+    ]
+};
